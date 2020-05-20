e8d4b7960d7d2aad9565f2fbc64c4ca17c5024b4
Yaroslav <yarik_watson@hotmail.com>
2020-05-20 08:53:59 +0300

    finish look at market

--- gulpfile.js
+++ gulpfile.js
@@ -36,11 +36,11 @@ function style() {
 function watch() {
   browserSync.init({
     server: {
-      baseDir: './'
+      baseDir: './',
     }
   })
   gulp.watch('./scss/**/*.scss', style);
-  gulp.watch('./*.html').on('change', browserSync.reload);
+  gulp.watch('./*/*.html').on('change', browserSync.reload);
   gulp.watch('./js/**/*.js').on('change', browserSync.reload);
 }
 
